dd9eea9594625b1f550246e433d1f701202e07ca
macoyuu <165467533+macoyuu@users.noreply.github.com>
2024-08-02 18:36:04 +0900

    いいね機能の修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,21 +1,25 @@
 class LikesController < ApplicationController
   before_action :set_user
-  
+
   def create
-    like = current_user.likes.build(to_user_id: params[:user_id]) # 外部キーの指定を修正
-    like.save
-    respond_to do |format|
-      format.js
+    like = current_user.likes.build(to_user_id: @user.id) # 外部キーの指定を修正
+    if like.save
+      like.check_matching # 状態が更新された後にコールバックを実行
+      respond_to do |format|
+        format.js
+      end
     end
   end
 
   def destroy
-    like = Like.find_by(to_user_id: params[:user_id], from_user_id: current_user.id)
+    like = Like.find_by(to_user_id: @user.id, from_user_id: current_user.id)
     like&.destroy
     respond_to do |format|
       format.js
     end
   end
+
+  private
 
   def set_user
     @user = User.find(params[:user_id])
--- app/models/like.rb
+++ app/models/like.rb
@@ -2,4 +2,14 @@ class Like < ApplicationRecord
   belongs_to :from_user, class_name: 'User'
   belongs_to :to_user, class_name: 'User'
   #has_many :messages
+
+  after_create :check_matching
+
+  def check_matching
+    if Like.exists?(from_user_id: self.to_user_id, to_user_id: self.from_user_id)
+      self.update(matching: true)
+      matching_like = Like.find_by(from_user_id: self.to_user_id, to_user_id: self.from_user_id)
+      matching_like.update(matching: true) if matching_like
+    end
+  end
 end
